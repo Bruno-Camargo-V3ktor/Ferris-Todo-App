e6e3c8957ea0d44b763ee54d9ebd387ffe0974b5
Bruno C. <v3ktoryt@gmail.com>
2025-05-29 18:46:55 -0300

    ✨Feat: Created a structure that manages the states of our app, and created an Enum, for possible errors involving our application as a whole

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,35 @@
+use models::{TodoListFilter, TodoToggleAction};
+use repository::{TodoRepo, TodoRepoError};
+
 pub mod models;
 pub mod repository;
+
+// Enums
+enum AppError {
+    TodoRepo(TodoRepoError),
+}
+
+// struct
+#[derive(Debug)]
+pub struct AppState {
+    pub selected_filter: TodoListFilter,
+    pub toggle_action: TodoToggleAction,
+    pub todo_repo: TodoRepo,
+}
+
+// Impls
+impl Default for AppState {
+    fn default() -> Self {
+        Self {
+            selected_filter: TodoListFilter::All,
+            toggle_action: TodoToggleAction::Check,
+            todo_repo: TodoRepo::default(),
+        }
+    }
+}
+
+impl From<TodoRepoError> for AppError {
+    fn from(value: TodoRepoError) -> Self {
+        Self::TodoRepo(value)
+    }
+}
